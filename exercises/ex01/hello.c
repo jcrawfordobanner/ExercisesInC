12f1ef407639842b1d4808f9c3087fec461dbd6b
jcrawfordobanner <jordan.crawford-obanner@students.olin.edu>
2019-01-31 10:02:55 -0500

    hw started

--- exercises/ex01/hello.c
+++ exercises/ex01/hello.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
+int add(int x, int y) {
+  return x+y;
+}
+
 int main() {
     printf("Hello, World!\n");
+    add(2,4);
+    int a = 3;
+    int b = 4;
+    int c = a + b;
+    printf("c is %d\n", c);
     return 0;
 }
---
+++ exercises/ex01/hello.s
@@ -0,0 +1,57 @@
+	.file	"hello.c"
+	.section	.text.unlikely,"ax",@progbits
+.LCOLDB0:
+	.text
+.LHOTB0:
+	.p2align 4,,15
+	.globl	add
+	.type	add, @function
+add:
+.LFB23:
+	.cfi_startproc
+	leal	(%rdi,%rsi), %eax
+	ret
+	.cfi_endproc
+.LFE23:
+	.size	add, .-add
+	.section	.text.unlikely
+.LCOLDE0:
+	.text
+.LHOTE0:
+	.section	.rodata.str1.1,"aMS",@progbits,1
+.LC1:
+	.string	"Hello, World!"
+.LC2:
+	.string	"c is %d\n"
+	.section	.text.unlikely
+.LCOLDB3:
+	.section	.text.startup,"ax",@progbits
+.LHOTB3:
+	.p2align 4,,15
+	.globl	main
+	.type	main, @function
+main:
+.LFB24:
+	.cfi_startproc
+	subq	$8, %rsp
+	.cfi_def_cfa_offset 16
+	movl	$.LC1, %edi
+	call	puts
+	movl	$7, %edx
+	movl	$.LC2, %esi
+	movl	$1, %edi
+	xorl	%eax, %eax
+	call	__printf_chk
+	xorl	%eax, %eax
+	addq	$8, %rsp
+	.cfi_def_cfa_offset 8
+	ret
+	.cfi_endproc
+.LFE24:
+	.size	main, .-main
+	.section	.text.unlikely
+.LCOLDE3:
+	.section	.text.startup
+.LHOTE3:
+	.ident	"GCC: (Ubuntu 5.4.0-6ubuntu1~16.04.10) 5.4.0 20160609"
+	.section	.note.GNU-stack,"",@progbits
