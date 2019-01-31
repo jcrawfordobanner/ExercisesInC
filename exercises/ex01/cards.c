485fa40030517b44b97e1c983b7ff6b69ac92804
jcrawfordobanner <jordan.crawford-obanner@students.olin.edu>
2019-01-31 10:52:24 -0500

    finished first part of homework

--- exercises/ex01/cards.c
+++ exercises/ex01/cards.c
@@ -1,38 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+
+/*
+Takes in a card name and returns the value associtated
+with the card name
+
+card_name: the last inputted card
+*/
+int cardValue(char card_name[]){
+  int val;
+  switch(card_name[0]) {
+    case 'K':
+    case 'Q':
+    case 'J':
+      val = 10;
+      break;
+    case 'A':
+      val = 11;
+      break;
+    default:
+      val = atoi(card_name);
+  }
+  return val;
+}
+
+
+/*
+Takes the card value and the last registered count
+and increases or decreases the count based upon the card value
+
+count: the last count val:the last card value
+*/
+int countaddr(int count, int val){
+  if ((val > 2) && (val < 7)) {
+    count++;
+  }
+
+  else if (val == 10) {
+    count--;
+  }
+  return count;
+}
+
+/*
+Gets the card name from the user
+
+card_name: place holder for the user's value
+*/
+char getEntry(char card_name[]){
+  puts("Enter the card_name: ");
+  scanf("%2s", card_name);
+  return card_name;
+}
+
+
+/*
+Counts cards by taking in a card from the user and calculating
+a value based upon the cards the user has inputted.
+*/
 int main()
 {
+    int val = 0;
     char card_name[3];
     int count = 0;
     while (card_name[0] != 'X') {
-      puts("Enter the card_name: ");
-      scanf("%2s", card_name);
-      int val = 0;
-      switch(card_name[0]) {
-        case 'K':
-        case 'Q':
-        case 'J':
-          val = 10;
-          break;
-        case 'A':
-          val = 11;
-          break;
-        case 'X':
-          continue;
-        default:
-          val = atoi(card_name);
-          if((val < 1) || (val > 10)) {
-            puts("I don;t understand that value!");
-            continue;
-          }
+      getEntry(card_name);
+      val = cardValue(card_name);
+      if((val < 1) || (val > 10)) {
+        puts("I don't understand that value!");
+        continue;
       }
-      if ((val > 2) && (val < 7)) {
-        count++;
-      }
-
-      else if (val == 10) {
-        count--;
-      }
+      count = countaddr(count,val);
       printf("Current count: %i\n", count);
     }
     return 0;
